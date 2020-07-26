74298d743a9c25c708bc8c6fab49acfb057d72be
vyrishkis <krasovskis@gmail.com>
2020-07-26 17:23:14 +0300

    list of months

---
+++ js/month.js
@@ -0,0 +1,14 @@
+const month = {
+    1: 'sausis',
+    2: 'vasaris',
+    3: 'kovas',
+    4: 'balandis',
+    5: 'gegužė',
+    6: 'biželis',
+    7: 'liepa',
+    8: 'rupjutis',
+    9: 'rugsėjis',
+    10:'spalis',
+    11: 'lapkritis',
+    12: 'gruodis'
+}
